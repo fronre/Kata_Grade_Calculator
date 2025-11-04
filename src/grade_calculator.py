a54cf03e6b8876f6fad44342bc4d90c95465e189
fronre <islamhala35@gmail.com>
2025-11-04 21:51:38 +0100

    refactoring

--- src/grade_calculator.py
+++ src/grade_calculator.py
@@ -4,11 +4,5 @@ def grade_calculator(scores: list[int]) -> str :
     if any(score < 0 for score in scores):
         return "Error: scores cannot be negative"
 
-    if scores == [1]:
-        return "F"
-    if scores == [2]:
-        return "F"
-    if scores == [3]:
-        return "F"
-    if scores == [4]:
+    if any (score <60 for score in scores):
         return "F"
--- testes/test_grade_calculator.py
+++ testes/test_grade_calculator.py
@@ -1,25 +1,13 @@
+import pytest
 from src.grade_calculator import grade_calculator
 
 
 def test_grade_calculator_is_empty_returns_error():
     assert grade_calculator([]) == "Error: no scores provided"
 
-
 def test_grade_calculator_returns_invalid_scores_below_0_returns_error():
     assert grade_calculator([-4]) == "Error: scores cannot be negative"
 
-
-def test_single_score_should_return_correct_grade1():
-    assert grade_calculator([1]) == "F"
-
-
-def test_single_score_should_return_correct_grade2():
-    assert grade_calculator([2]) == "F"
-
-
-def test_single_score_should_return_correct_grade3():
-    assert grade_calculator([3]) == "F"
-
-
-def test_single_score_should_return_correct_grade4():
-    assert grade_calculator([4]) == "F"
+@pytest.mark.parametrize("score", [1, 2, 3, 4, 5, 59])
+def test_scores_below_60_return_F(score):
+      assert grade_calculator([score]) == "F"
