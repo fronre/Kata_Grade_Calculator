b29649e3a2899f2703c5275425a81d9b5d0bb24b
fronre <islamhala35@gmail.com>
2025-11-04 22:16:57 +0100

    averag<60 ==f

--- src/grade_calculator.py
+++ src/grade_calculator.py
@@ -1,3 +1,6 @@
+from numpy.ma.extras import average
+
+
 def grade_calculator(scores: list[int]) -> str :
     if scores == []:
         return "Error: no scores provided"
@@ -6,3 +9,6 @@ def grade_calculator(scores: list[int]) -> str :
 
     if any (score <60 for score in scores):
         return "F"
+
+    if average(scores) < 60:
+         return "F"
--- testes/test_grade_calculator.py
+++ testes/test_grade_calculator.py
@@ -11,3 +11,6 @@ def test_grade_calculator_returns_invalid_scores_below_0_returns_error():
 @pytest.mark.parametrize("score", [1, 2, 3, 4, 5, 59])
 def test_scores_below_60_return_F(score):
       assert grade_calculator([score]) == "F"
+
+def test_scores_average_to_F():
+    assert grade_calculator([50, 55, 58]) == "F"
